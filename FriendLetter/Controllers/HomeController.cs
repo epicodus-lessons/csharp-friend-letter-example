97171aec6c450e84d904b027bfd256e20f9cc007
Courtney Phillips <courtney@epicodus.com>
2018-11-19 15:02:00 -0800

    Add second 'goodbye' route to controller.

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ namespace FriendLetter.Controllers
 {
   public class HomeController : Controller
   {
+    
     public string Hello() { return "Hello friend!"; }
+
+    public string Goodbye() { return "Goodbye friend."; }
+
   }
 }
